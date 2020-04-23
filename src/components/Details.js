9dd0cf3ae13e91924f217b42bf1efe91a7318571
souvik <p24souvik@gmail.com>
2020-04-24 02:14:19 +0530

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Product from './Product';
+
+export default class Details extends Component {
+  render() {
+    return (
+        
+     
+    	<div>
+    		<h3> Hello from Details </h3>
+    	</div>
+      
+    )
+  }
+}
